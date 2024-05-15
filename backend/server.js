053eaf1e3d2dfee921b59501f80e2b8444e84879
Acurrie4 <156128305+Acurrie4@users.noreply.github.com>
2024-05-15 18:45:05 -0400

    Server and package.json updated

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config();
-const PORT = process.env.PORT || 5000;
+const cors = require('cors');
 
-const userRouter = require('./routes/userRoutes');
-const reviewRouter = require('./routes/reviewRoutes');
+dotenv.config();
 
 const app = express();
-app.use(express.json()); // Middleware to parse JSON
 
-const cors = require('cors');
+app.use(express.json()); // Middleware to parse JSON
 app.use(cors({
-  origin: 'http://localhost:3000' // Frontend server url
+  origin: 'http://localhost:3000' 
 }));
 
 // MongoDB Connection
@@ -20,13 +17,19 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('DB connection successful!'));
+.then(() => console.log('DB connection successful!'))
+.catch(err => console.error('MongoDB connection error:', err)); // Add error handling
 
-// Routes
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+
 app.use('/api/users', userRouter);
 app.use('/api/reviews', reviewRouter);
 
+// Export the app for use in other files such as during testing
+module.exports = app;
 
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
